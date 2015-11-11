a82292ae35fe676f8e37dd2cdbffbd0a4fac4c18
jverce <juanjov@gmail.com>
2015-11-11 01:00:03 -0300

    Add symbolic operators

--- lectures/.worksheet/src/week3.scala
+++ lectures/.worksheet/src/week3.scala
@@ -1,10 +1,8 @@
 object week3 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(43); 
   val a = new Rational(1, 3);System.out.println("""a  : Rational = """ + $show(a ));$skip(29); 
   val b = new Rational(5, 7);System.out.println("""b  : Rational = """ + $show(b ));$skip(29); 
-  val c = new Rational(3, 2);System.out.println("""c  : Rational = """ + $show(c ));$skip(18); val res$0 = 
-  a.sub(b).sub(c);System.out.println("""res0: Rational = """ + $show(res$0));$skip(11); val res$1 = 
-  c.max(a);System.out.println("""res1: Rational = """ + $show(res$1));$skip(26); 
-  val d = new Rational(1);System.out.println("""d  : Rational = """ + $show(d ))}
+  val c = new Rational(3, 2);System.out.println("""c  : Rational = """ + $show(c ));$skip(8); val res$0 = 
+  a / c;System.out.println("""res0: Rational = """ + $show(res$0))}
 }
 
 class Rational(x: Int, y: Int) {
@@ -20,19 +18,29 @@ class Rational(x: Int, y: Int) {
   val numer = x / g
   val denom = y / g
     
-  def less(that: Rational): Boolean =
+  def < (that: Rational): Boolean =
     this.numer * that.denom < that.numer * this.denom
   
-  def max(that: Rational): Rational = if (this.less(that)) that else this
+  def max(that: Rational): Rational = if (this < that) that else this
   
-  def add(that: Rational): Rational =
+  def + (that: Rational): Rational =
     new Rational(
       this.numer * that.denom + that.numer * this.denom,
       this.denom * that.denom)
   
-  def neg: Rational = new Rational(-this.numer, this.denom)
+  def unary_- = new Rational(-this.numer, this.denom)
   
-  def sub(that: Rational): Rational = this.add(that.neg)
+  def - (that: Rational): Rational = this + -that
+  
+  def * (that: Rational): Rational =
+    new Rational(
+      this.numer * that.numer,
+      this.denom * that.denom)
+  
+  def / (that: Rational): Rational =
+    new Rational(
+      this.numer * that.denom,
+      this.denom * that.numer)
   
   override def toString =
     this.numer +
--- lectures/src/week3.sc
+++ lectures/src/week3.sc
@@ -2,9 +2,7 @@ object week3 {
   val a = new Rational(1, 3)                      //> a  : Rational = 1/3
   val b = new Rational(5, 7)                      //> b  : Rational = 5/7
   val c = new Rational(3, 2)                      //> c  : Rational = 3/2
-  a.sub(b).sub(c)                                 //> res0: Rational = -79/42
-  c.max(a)                                        //> res1: Rational = 3/2
-  val d = new Rational(1)                         //> d  : Rational = 1
+  a / c                                           //> res0: Rational = 2/9
 }
 
 class Rational(x: Int, y: Int) {
@@ -20,19 +18,29 @@ class Rational(x: Int, y: Int) {
   val numer = x / g
   val denom = y / g
     
-  def less(that: Rational): Boolean =
+  def < (that: Rational): Boolean =
     this.numer * that.denom < that.numer * this.denom
   
-  def max(that: Rational): Rational = if (this.less(that)) that else this
+  def max(that: Rational): Rational = if (this < that) that else this
   
-  def add(that: Rational): Rational =
+  def + (that: Rational): Rational =
     new Rational(
       this.numer * that.denom + that.numer * this.denom,
       this.denom * that.denom)
   
-  def neg: Rational = new Rational(-this.numer, this.denom)
+  def unary_- = new Rational(-this.numer, this.denom)
   
-  def sub(that: Rational): Rational = this.add(that.neg)
+  def - (that: Rational): Rational = this + -that
+  
+  def * (that: Rational): Rational =
+    new Rational(
+      this.numer * that.numer,
+      this.denom * that.denom)
+  
+  def / (that: Rational): Rational =
+    new Rational(
+      this.numer * that.denom,
+      this.denom * that.numer)
   
   override def toString =
     this.numer +
